5668f2374f31d52a5855da6a3ba554388fbf4083
Litian-25 <kkyh2525@ezweb.ne.jp>
2025-07-07 11:26:10 +0900

    71の途中

--- Source/Aura/Private/Characters/AuraCharacter.cpp
+++ Source/Aura/Private/Characters/AuraCharacter.cpp
@@ -63,5 +63,5 @@ void AAuraCharacter::InitAbilityActorInfo()
 			AuraHUD->InitOverlay(AuraPlayerController, AuraPlayerState, AbilitySystemComponent, AttributeSet);
 		} 
 	}
-	InitializePrimaryAttributes();
+	InitializeDefaultAttributes();
 }
